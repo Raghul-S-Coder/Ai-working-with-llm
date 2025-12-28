ab4440990ca0d6dd6ec985127d801b8f30ba7b90
Raghul Coder <eceraghulksrct@gmail.com>
2025-12-28 11:29:28 +0530

    Fixed the prompt issue

--- llm_prompt_injection.py
+++ llm_prompt_injection.py
@@ -3,8 +3,10 @@ from llm import _initialize_llm
 
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.messages import SystemMessage, HumanMessage
 
-CLASSIFIER_SYSTEM_PROMPT = ChatPromptTemplate.from_template("""
+CLASSIFIER_SYSTEM_PROMPT = SystemMessage(
+    content="""
     You are an intent classifier for a chat system.
     
     Classify the user's intent into exactly ONE of the following values:
@@ -20,21 +22,18 @@ CLASSIFIER_SYSTEM_PROMPT = ChatPromptTemplate.from_template("""
       "intent": "{{<one of the allowed values>}}",
       "confidence": <float between 0 and 1>
     }
-    
-    {user_query}
 """)
 
 
 llm = _initialize_llm()
-llm_classify_chain = CLASSIFIER_SYSTEM_PROMPT | llm | StrOutputParser
-llm_chain = ChatPromptTemplate.from_template("{user_query}") | llm | StrOutputParser
+llm_chain = ChatPromptTemplate.from_template("Here is the user query: {user_query}") | llm | StrOutputParser()
 
 
 def classify_intent(user_input: str):
-    response = llm_classify_chain.invoke({"user_query": user_input})
-    print(response)
+    messages = [CLASSIFIER_SYSTEM_PROMPT] + [HumanMessage(content=user_input)]
+    response = llm.invoke(messages)
     try:
-        data = json.loads(str(response))
+        data = json.loads(str(response.content))
         return data
     except json.JSONDecodeError:
         return None
@@ -64,8 +63,10 @@ def handle_user_message(user_input: str):
     if intent == "role_change":
         return "Role changes are restricted."
 
-    # Normal flow
-    return call_main_llm(user_input)
+    if intent == "normal" or intent == "context_reset":
+        return call_main_llm(user_input)
+
+    return "invalid or malicious input, try again with different question!"
 
 
 if __name__ == "__main__":
